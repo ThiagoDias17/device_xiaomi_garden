8c32f88d0f932ecaa0fdff8bc189a898a077190a
Thiago Dias <tiago717292@gmail.com>
2024-06-23 13:30:37 +0000

    Garden: bringup to dotOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_garden.mk
+   $(LOCAL_DIR)/dot_garden.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_garden-userdebug \
-    lineage_garden-eng \
-    lineage_garden-user
+    dot_garden-userdebug \
+    dot_garden-eng \
+    dot_garden-user
